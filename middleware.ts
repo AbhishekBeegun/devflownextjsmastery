d738d5edd73079573fb581335b107c92ff462a77
Abhishek Beegun <abhishekbeegun0642@gmail.com>
2023-11-04 18:42:30 +0400

    Need to create community page

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,12 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
       publicRoutes : [
             '/',
-            '/api/webhook'
+            '/api/webhook',
             // '/question/:id',
             // '/tags',
             // '/tags/:id',
             // '/profile/:id',
-            // '/community',
+            '/community',
             // '/jobs'
       ],
       ignoredRoutes : [
